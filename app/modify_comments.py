9917714f921e96829133452d433bd1763c89e5b7
jeffwu0724 <jeffwu0724@gmail.com>
2025-03-15 21:18:21 -0700

    create a method to interact with database, also create a method to fetch all items

---
+++ app/modify_comments.py
@@ -0,0 +1,36 @@
+# Impliment edit, add, and delete functionality to comments. Update the database accordingly when these APIs are called.
+
+# Requirements:
+# Edit text of existing comments 
+# Add a comment, with new text (from “Admin” user), with the current time
+# Delete existing comments 
+# List all comments
+
+import boto3
+import json
+import decimal
+
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table("comments")
+
+# Helper class to convert Decimal types to float for JSON serialization
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            return float(o) if o % 1 else int(o)
+        return super(DecimalEncoder, self).default(o)
+
+# List all comments
+def fetch_all_comments():
+    response = table.scan()
+    # print(response)
+    items = response["Items"]
+    if items:
+        return {"statusCode": 200, "body": json.dumps(items, cls=DecimalEncoder)}
+    else:
+        return {"statusCode": 404, "body": "comments not found"}
+    
+if __name__ == "__main__":
+    
+    result = fetch_all_comments()
+    print(result)
